e3e3c2f9120f7dada308042c1d3577303cfe9f11
RRP12 <rushikeshpatil8208@gmail.com>
2025-10-23 13:45:25 +0530

    add the feature for promet metadata

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,35 @@ import { Hono } from 'hono';
 import { convertToModelMessages, generateObject, generateText, streamText } from 'ai';
 import { createWorkersAI } from 'workers-ai-provider';
 import { Ai } from '@cloudflare/workers-types';
-
+import z from "zod";
+import { createClient } from '@supabase/supabase-js'
+import { env, getRuntimeKey } from 'hono/adapter'
+import { uuid } from 'zod/v4';
 type Env = {
   AI: Ai;
 };
 
 const app = new Hono<{ Bindings: Env }>();
+
+
+
+const MCPInputSchema = z
+  .union([
+    z.object({ fileName: z.string().min(1) }), // represents uploaded file
+    z.object({
+      serverLink: z.string().url("Must be a valid URL"),
+      apiKey: z.string().min(1, "API Key is required"),
+    }),
+  ])
+  .refine(
+    (data) => {
+      const hasFile = "fileName" in data && data.fileName;
+      const hasServer = "serverLink" in data && data.serverLink && data.apiKey;
+      return hasFile !== hasServer;
+    },
+    { message: "Provide either a file or MCP server link + API key" }
+  );
+
 
 app.options('*', async (c) => {
   return new Response(null, {
@@ -21,7 +44,15 @@ app.options('*', async (c) => {
   });
 });
 
-app.post('/', async (c) => {
+app.post('/template', async (c) => {
+
+
+  let workersai = createWorkersAI({ binding: c.env.AI });
+
+  const { SUPABASE_URL, SUPABASE_ANON_KEY } = env<{ SUPABASE_URL: string, SUPABASE_ANON_KEY: string }>(c)
+  const supabase = createClient("https://sfaqwyumdxebchjxyyyv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmYXF3eXVtZHhlYmNoanh5eXl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxODA4MjAsImV4cCI6MjA3NTc1NjgyMH0.c6lfauF-dlq0txeC0FiBbBQ5HuNDNxTYTsd0AEZKshU");
+
+
 
 
 
@@ -29,33 +60,94 @@ app.post('/', async (c) => {
   const { messages, text }: { messages: any, text: string } =
     await c.req.json();
 
-  const workersai = createWorkersAI({ binding: c.env.AI });
-
-
 
   console.log("customKey", text)
 
+  //   const { text: copy } = await generateText({
+  //     model: workersai('@cf/meta/llama-4-scout-17b-16e-instruct'),
+
+  //     prompt: `Write a System prompt for ${text} the business requirements and also focus of the user intent 
+
+  // <Identity>
+  // You are a helpful <Persona>Who or what the model is acting as. Also called "role" or "vision</Persona> who is an expert in ${text}
+  // </Identity>
+  // <Instructions>
+  //  Only output a single word in your response with no additional formatting
+  //   or commentary.
+  //  Your response should only be one of the words "Positive", "Negative", or
+  //   "Neutral" depending on the sentiment of the product review you are given.
+  // </Instructions>
+  //     }
+
+
+  //     <Tone>Respond in a casual and technical manner.</Tone>
+  //     exmple : 
+
+  //     <OBJECTIVE_AND_PERSONA>
+  // You are a [insert a persona, such as a "math teacher" or "automotive expert"]. Your task is to...
+  // </OBJECTIVE_AND_PERSONA>
+
+  // <INSTRUCTIONS>
+  // To complete the task, you need to follow these steps:
+  // 1.
+  // 2.
+  // ...
+  // </INSTRUCTIONS>
+
+  // ------------- Optional Components ------------
+
+  // <CONSTRAINTS>
+  // Dos and don'ts for the following aspects
+  // 1. Dos
+  // 2. Don'ts
+  // </CONSTRAINTS>
+
+  // <CONTEXT>
+  // The provided context
+  // </CONTEXT>
+
+  // <OUTPUT_FORMAT>
+  // The output format must be
+  // 1.
+  // 2.
+  // ...
+  // </OUTPUT_FORMAT>
+
+  // <FEW_SHOT_EXAMPLES>
+  // Here we provide some examples:
+  // 1. Example #1
+  // Input:
+  // Thoughts:
+  // Output:
+  // ...
+  // </FEW_SHOT_EXAMPLES>
+
+  // <RECAP>
+  // Re-emphasize the key aspects of the prompt, especially the constraints, output format, etc.
+  // </RECAP>
+  //     `,
+  //   });
   const { text: copy } = await generateText({
     model: workersai('@cf/meta/llama-4-scout-17b-16e-instruct'),
 
     prompt: `Write a System prompt for ${text} the business requirements and also focus of the user intent 
-   
+ 
 <Identity>
 You are a helpful <Persona>Who or what the model is acting as. Also called "role" or "vision</Persona> who is an expert in ${text}
 </Identity>
 <Instructions>
- Only output a single word in your response with no additional formatting
-  or commentary.
- Your response should only be one of the words "Positive", "Negative", or
-  "Neutral" depending on the sentiment of the product review you are given.
+Only output a single word in your response with no additional formatting
+or commentary.
+Your response should only be one of the words "Positive", "Negative", or
+"Neutral" depending on the sentiment of the product review you are given.
 </Instructions>
-    }
+  }
 
 
-    <Tone>Respond in a casual and technical manner.</Tone>
-    exmple : 
+  <Tone>Respond in a casual and technical manner.</Tone>
+  exmple : 
 
-    <OBJECTIVE_AND_PERSONA>
+  <OBJECTIVE_AND_PERSONA>
 You are a [insert a persona, such as a "math teacher" or "automotive expert"]. Your task is to...
 </OBJECTIVE_AND_PERSONA>
 
@@ -97,26 +189,68 @@ Output:
 <RECAP>
 Re-emphasize the key aspects of the prompt, especially the constraints, output format, etc.
 </RECAP>
-    `,
+  `,
   });
-
-
 
   console.log("copy", copy)
 
-  const { object } = await generateObject({
+
+  // const { data: projects, error: fetchErr } = await supabase
+  //   .from('Project')
+  //   .select('id, name')
+  //   .eq('name', projectName)
+  //   .limit(1);
+
+  // if (fetchErr) throw fetchErr;
+
+  // if (!projects || projects.length === 0) {
+  //   throw new Error('Project not found');
+  // }
+
+  // const projectId = projects[0].id;
+
+
+
+
+
+  const { object: PromptMetaData } = await generateObject({
     model: workersai('@cf/meta/llama-4-scout-17b-16e-instruct'),
-    output: 'enum',
-    enum: ['action', 'comedy', 'drama', 'horror', 'sci-fi'],
-    prompt:
-      'Classify the genre of this movie plot: ' +
-      '"A group of astronauts travel through a wormhole in search of a ' +
-      'new habitable planet for humanity."',
+    schema: z.object({
+      Identity: z.string(),
+      Instructions: z.string(),
+      Tone: z.string(),
+    }),
+    prompt: `Evaluate this System prompt for Business requirements and user intent:
+   and extract the proper Identity, Instructions  and Tone for the ai agent for the business
+
+    Prompt to evaluate: ${copy}`,
   });
 
 
-  console.log("object", object)
 
+
+
+  console.log("PromptMetaData", PromptMetaData);
+
+  const { data: ProjectMetadata, error: projectError } = await supabase
+    .from('ProjectMetadata')
+    .insert([
+      { id: "b2103e6c-487b-499c-a652-7ce63155ad7b", identity: PromptMetaData.Identity, instructions: PromptMetaData.Instructions, tone: PromptMetaData.Tone },
+    ])
+    .select()
+
+
+  console.log("ProjectMetadata", ProjectMetadata)
+  console.log("projectError", projectError)
+
+
+
+});
+
+
+app.post('/chat', async (c) => {
+
+  let workersai = createWorkersAI({ binding: c.env.AI });
 
 
   const result = await streamText({
@@ -135,4 +269,123 @@ Re-emphasize the key aspects of the prompt, especially the constraints, output f
   });
 });
 
+
+
 export default app;
+
+
+
+
+
+
+// /template 
+
+// /chat
+
+
+// import {
+//   convertToModelMessages,
+//   createUIMessageStream,
+//   createUIMessageStreamResponse,
+//   generateText,
+//   ModelMessage,
+//   PrepareStepFunction,
+//   stepCountIs,
+//   streamObject,
+//   streamText,
+//   tool,
+//   UIMessage,
+// } from "ai";
+// import { createMistral } from "@ai-sdk/mistral";
+// import { generateObject } from "ai";
+// import z from "zod";
+// import fs from "fs";
+// import path from "path";
+// import { createGoogleGenerativeAI } from "@ai-sdk/google";
+// import { createGateway } from "@ai-sdk/gateway";
+// const google = createGoogleGenerativeAI({
+//   apiKey: "AIzaSyD7qTXxvPibDvKhU_hduloMMap9B6YuHzM",
+// });
+
+// const gateway = createGateway({
+//   apiKey: process.env.AI_GATEWAY_API_KEY, // the default environment variable for the API key
+//   baseURL: "https://ai-gateway.vercel.sh/v1/ai", // the default base URL
+// });
+// const mistral = createMistral({
+//   apiKey: "cAdRTLCViAHCn0ddFFEe50ULu04MbUvZ",
+// });
+
+// export const maxDuration = 30;
+
+
+// export async function POST(req: Request) {
+//   const { messages, customKey }: { messages: UIMessage[]; customKey: string } =
+//     await req.json();
+
+//   // First step: Generate marketing copy
+
+
+//   // Perform quality check on copy
+//   const { object: PromptMetaData } = await generateObject({
+//     model: mistral("mistral-medium-latest"),
+//     schema: z.object({
+//       Identity: z.string(),
+//       Instructions: z.string(),
+//       Tone: z.string(),
+//     }),
+//     prompt: `Evaluate this System prompt for Business requirements and user intent:
+//    and extract the proper Identity, Instructions  and Tone for the ai agent for the business
+
+//     Prompt to evaluate: ${copy}`,
+//   });
+
+//   console.log("PromptMetaData", PromptMetaData);
+
+
+//   // If quality check fails, regenerate with more specific instructions
+
+//   // const stream = createUIMessageStream({
+//   //   originalMessages: messages,
+//   //   execute: async ({ writer }) => {
+//   //     const result = streamText({
+//   //       system:
+//   //         "You are an AI agent builder. When users ask you to create or develop an AI agent for a business, you must first gather MCP server information by calling the gatherMcpInformation tool ",
+//   //       model: mistral("mistral-large-latest"),
+//   //       messages: convertToModelMessages(messages),
+//   //       tools: {
+//   //         gatherMcpInformation: tool({
+//   //           description:
+//   //             "when asked to develope agnet show the Option to upload the Open Api schema file or connect the mcp server Link and API key ",
+//   //           inputSchema: MCPInputSchema,
+//   //           outputSchema: z.string(),
+//   //         }),
+//   //       },
+//   //       stopWhen: stepCountIs(5),
+
+//   //       toolChoice: "required",
+//   //       prepareStep: ({ steps, stepNumber, messages }) => {
+//   //         if (stepNumber > 10) {
+//   //           return {
+//   //             toolChoice: {
+//   //               type: "tool",
+//   //               toolName: "gatherMcpInformation",
+//   //             },
+//   //           };
+//   //         }
+
+//   //         return undefined;
+//   //       },
+//   //     });
+
+//   //     let res = result.toolCalls?.then((toolCalls) => {
+//   //       console.log("toolCalls", toolCalls);
+//   //     });
+
+//   //     console.log("toolscal", res);
+//   //     writer.merge(result.toUIMessageStream({ originalMessages: messages }));
+//   //   },
+//   // });
+
+//   return createUIMessageStreamResponse({ stream });
+// }
+
